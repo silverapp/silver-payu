34c8947749eda3bf34df12f78020086470305e8a
Vlad Temian <vladtemian@gmail.com>
2017-11-09 15:35:15 +0200

    Fix url compatibilities issues with django 1.11

--- silver_payu/urls.py
+++ silver_payu/urls.py
@@ -13,15 +13,16 @@
 # limitations under the License.
 
 
-from django.conf.urls import include, url, patterns
+from django.conf.urls import include, url
 
 from silver.views import pay_transaction_view, complete_payment_view
 
 
-urlpatterns = patterns('',
-                       (r'^', include('payu.urls')),
+urlpatterns = [
+    url(r'^', include('payu.urls')),
 
-                       url(r'pay/(?P<token>[0-9a-zA-Z-_\.]+)/$',
-                           pay_transaction_view, name='payment'),
-                       url(r'pay/(?P<token>[0-9a-zA-Z-_\.]+)/complete$',
-                           complete_payment_view, name='payment-complete'))
+    url(r'pay/(?P<token>[0-9a-zA-Z-_\.]+)/$',
+        pay_transaction_view, name='payment'),
+    url(r'pay/(?P<token>[0-9a-zA-Z-_\.]+)/complete$',
+        complete_payment_view, name='payment-complete')
+]
