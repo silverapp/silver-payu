bc2cdc4dbbe049465dbf79533d18886bd9752be3
Vlad Temian <vladtemian@gmail.com>
2017-11-13 18:30:32 +0200

    Update django-payu-ro version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 from silver_payu import __version__ as version
 
-install_requires = ['django-payu-ro>=1.0.5']
+install_requires = ['django-payu-ro>=1.0.6']
 
 
 def read(fname):
